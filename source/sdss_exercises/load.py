4832947b872b051b0bdb8c3973099c543b935c53
hannahm <hmiddleton8@googlemail.com>
2018-11-20 16:57:17 +1100

    starting new branch, will read in and print number of hdu for each

--- source/sdss_exercises/load.py
+++ source/sdss_exercises/load.py
@@ -1,22 +1,26 @@
+#!/usr/bin/env python
+
 from astropy.io import fits
-import argparser
+import argparse
 
 
 parser=argparse.ArgumentParser(description="...", usage="...")
-parser.add_argument("-f", "--numfiles", type=int, choices=range(100))
+
+parser.add_argument("-f", "--files", type=str, required = True, nargs="+", \
+                    dest="filename", \
+                    help = "a list of files to read in from")
 
 args =parser.parse_args()
 
+filename = args.filename
 
-filename = "spec-3949-55650-0042.fits"
+# file name should only be a list
 
-hdu_list = fits.open(filename)
-hdu1 = hdu_list[0] # FITS HDU counting is from 1
-print(f"This file has {len(hdu_list)} HDUs.") print(hdu1.header) # prints header print(hdu_list.info())
-object of type HDUList, can treat at list of HDUs
-hdu_list.info() returns structure of file (no. of HDUs, type of each, dimensions, etc.)
- # print primary HDU header
-#
-for key, value in hdu1.header.items(): ! print("{0} = {1}".format(key, value))
-generator to loop over each header item
-  hdu_list.close()
+
+
+for i in range(len(filename)):
+    hdu_list = fits.open(filename[i])
+   
+    print ((hdu_list))
+
+
